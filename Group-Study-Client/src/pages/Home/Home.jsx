091e3776e84043350d3e4487634e023645ad86b6
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2025-04-21 01:39:24 +0600

    group study server basic setup

--- Group-Study-Client/src/pages/Home/Home.jsx
+++ Group-Study-Client/src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Banner from './Banner'
+import Faq from './Faq'
 
 const Home = () => {
   return (
-    <div className='mb-6 rounded-3xl'>
+    <div className='mb-6'>
       
-      <Banner></Banner>
+     <div className='mb-6'>
+     <Banner></Banner>
+     </div>
+      <div className='mb-6'>
+      <Faq></Faq>
+      </div>
+
     </div>
   )
 }
