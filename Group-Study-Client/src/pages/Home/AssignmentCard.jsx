f4a139862ed8e602b2d524f9bbad9bacb759dc41
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2025-04-21 17:46:26 +0600

    added assignment info into assignment card

--- Group-Study-Client/src/pages/Home/AssignmentCard.jsx
+++ Group-Study-Client/src/pages/Home/AssignmentCard.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
-const AssignmentCard = () => {
+const AssignmentCard = ({assignment}) => {
+    const { title,  description,marks,thumbnail,difficulty,dueDate}=assignment;
   return (
-    <div>
-      
+    <div className="card bg-base-100 w-96 shadow-sm">
+    <div className="card-body">
+      <h2 className="card-title">{title}</h2>
+      <p>{description.split(" ").slice(0,5).join(" ")}</p>
+      <h4>{marks}</h4>
+      <h4>{difficulty}</h4>
+      <h2>{dueDate}</h2>
     </div>
+    <figure>
+      <img
+        src={thumbnail}
+        alt="assignment" />
+    </figure>
+  </div>
   )
 }
 
